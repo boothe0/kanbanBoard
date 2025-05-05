6d4a3bf537f7728d1a74050696b290365f902cdf
Elizabeth <elizabethbooth05@gmail.com>
2025-05-05 00:53:04 -0700

    userAddBoard

--- board.ts
+++ board.ts
@@ -12,13 +12,72 @@ const savedData = localStorage.getItem("tableOne");
 if (savedData) {
   Object.assign(tableOne, JSON.parse(savedData));
 }
+//localStorage.removeItem("tableOne");
 
-if (firstCard) {
-  tableOne.status = firstCard.children[0].innerHTML;
-  tableOne.name = firstCard.children[1].innerHTML;
-  tableOne.description = firstCard.children[2].innerHTML;
-  console.log(tableOne.status);
-  console.log(tableOne.name);
-  console.log(tableOne.description);
+function makeTheCard(
+  status: string,
+  title: string,
+  description: string,
+  assignee: string,
+  due_date: Date
+) {
+  const card = document.getElementById("boardCards");
+  const cardDivder = document.createElement("div");
+  cardDivder.classList.add("card-divider");
+  cardDivder.classList.add("align-center");
+  // set the inner html as the passed in param from the submit button
+  cardDivder.innerHTML = status;
+  // for loading purposes
+  tableOne.status = status;
+  const cardSectionName = document.createElement("div");
+  cardSectionName.classList.add("card-section");
+  // set the inner html as the passed in param from the submit button
+  cardSectionName.innerHTML = title;
+  // for loading purposes
+  tableOne.name = title;
+  const cardSectionDescription = document.createElement("div");
+  cardSectionDescription.classList.add("card-section");
+  // set the inner html as the passed in param from the submit button
+  cardSectionDescription.innerHTML = description;
+  // for loading purposes
+  tableOne.description = description;
+  tableOne.assignee = assignee;
+  tableOne.due_date = due_date;
+  // appending all children to the card
+  card?.appendChild(cardDivder);
+  card?.appendChild(cardSectionName);
+  card?.appendChild(cardSectionDescription);
+  console.log("Clicked!");
   localStorage.setItem("tableOne", JSON.stringify(tableOne));
+}
+
+(window as any).fillInBoard = function fillInBoard() {
+  const status = document.getElementById("status") as HTMLInputElement;
+  const statusValue = status.value;
+  const title = document.getElementById("title") as HTMLInputElement;
+  const titleValue = title.value;
+  const description = document.getElementById(
+    "description"
+  ) as HTMLInputElement;
+  const descriptionValue = description.value;
+  const assignee = document.getElementById("assignee") as HTMLInputElement;
+  const assigneeValue = assignee.value;
+  const dueDate = document.getElementById("dueDate") as HTMLInputElement;
+  const dueDateValue = new Date(dueDate.value);
+  makeTheCard(
+    statusValue,
+    titleValue,
+    descriptionValue,
+    assigneeValue,
+    dueDateValue
+  );
+  closeModal();
+};
+
+function closeModal() {
+  const modal = document.getElementById("modal1");
+  if (modal) {
+    // @ts-ignore
+    $(modal).foundation("close");
+  }
 }
--- dist/list.js
+++ dist/list.js
@@ -7,15 +7,20 @@ function fillInStatus() {
     const tableList2 = document.createElement("td");
     const tableList3 = document.createElement("td");
     const tableList4 = document.createElement("td");
+    const tableList5 = document.createElement("td");
     table === null || table === void 0 ? void 0 : table.appendChild(tableRow);
     tableRow === null || tableRow === void 0 ? void 0 : tableRow.appendChild(tableList1);
     tableRow === null || tableRow === void 0 ? void 0 : tableRow.appendChild(tableList2);
     tableRow === null || tableRow === void 0 ? void 0 : tableRow.appendChild(tableList3);
     tableRow === null || tableRow === void 0 ? void 0 : tableRow.appendChild(tableList4);
+    tableRow === null || tableRow === void 0 ? void 0 : tableRow.appendChild(tableList5);
     if (tableOne) {
         tableList1.innerHTML = tableOne.name;
         tableList2.innerHTML = tableOne.status;
         tableList3.innerHTML = tableOne.description;
+        tableList4.innerHTML = tableOne.assignee;
+        const date = new Date(tableOne.due_date);
+        tableList5.innerHTML = date.toLocaleDateString();
     }
 }
 fillInStatus();
